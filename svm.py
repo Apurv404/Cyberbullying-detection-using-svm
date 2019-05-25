ff9c452ac2af39dd47a14d8b85156dd0bfccc145
Apurv404 <apurv.choudhari1@gmail.com>
2019-05-26 01:39:19 +0530

    Update svm.py

--- svm.py
+++ svm.py
@@ -1,11 +1,8 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt  
 from sklearn.svm import SVC
 from sklearn import metrics
 from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.metrics import precision_recall_fscore_support
-#%matplotlib inline
 
 bankdata = pd.read_csv("features1.tsv",sep='\t')
 bankdata.shape
@@ -19,12 +16,8 @@ svclassifier = SVC(C=1.0,kernel='rbf')
 svclassifier.fit(X_train, y_train)
 y_pred = svclassifier.predict(X_test)
 
-
 print("Accuracy = ",metrics.accuracy_score(y_test,y_pred))
 print(precision_recall_fscore_support(y_test, y_pred, average='binary'))
 print(precision_recall_fscore_support(y_test, y_pred, average='micro'))
 print(precision_recall_fscore_support(y_test, y_pred, average='macro'))
 print(precision_recall_fscore_support(y_test, y_pred, average='weighted'))
-
-
-
